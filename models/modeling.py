a78bc0986dddeafe64f5e5276c36a224051ab3ee
jungjaewon <woodcook486@naver.com>
2024-07-26 22:46:46 +0900

    change loading weight part

--- models/modeling.py
+++ models/modeling.py
@@ -39,7 +39,8 @@ def np2th(weights, conv=False):
     """Possibly convert HWIO to OIHW."""
     if conv:
         weights = weights.transpose([3, 2, 0, 1])
-    return torch.from_numpy(weights)
+    #return torch.from_numpy(weights)
+    return weights
 
 
 def swish(x):
